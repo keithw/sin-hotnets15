0f8b3bf8ccda29dcb618c875c3fe4282c42f84e5
Greg Hill <gdh39@cornell.edu>
2015-07-14 22:41:17 -0700

    percent matching srtf graph

--- plots/gen_data.sh
+++ plots/gen_data.sh
@@ -7,7 +7,7 @@ NUM_TRIALS=1000
 DIE_SIZE=10
 
 echo '"num users" "Market" "Ideal TCP"' > $DELAY_RATIO_FILE
-echo "#num users | ratio of srtf matching scenarios" > $SRTF_RATIO_FILE
+echo '"num users" "Market" "Ideal TCP"' > $SRTF_RATIO_FILE
 for i in `seq 2 10`;
         do
         echo -n $i >> $DELAY_RATIO_FILE
@@ -17,7 +17,7 @@ for i in `seq 2 10`;
             $SIN_LOCATION --num-users=$i --num-trials=$NUM_TRIALS --die-size=$DIE_SIZE $user_type > temp
 
             cat temp | grep "average delay" | sed s/'average delay ratio'/' '/g | tr -d '\n' >> $DELAY_RATIO_FILE
-            cat temp | grep "scenarios matched" | cut -d "(" -f2 | cut -d ")" -f1 >>$SRTF_RATIO_FILE
+            cat temp | grep "scenarios matched" | cut -d "(" -f2 | cut -d ")" -f1 | tr -d '\n' >>$SRTF_RATIO_FILE
             done
 
         echo "" >> $DELAY_RATIO_FILE
