f55e74c9d0b95ca598e4e8dfa56faef82e3ff2c7
Greg Hill <gdh39@cornell.edu>
2015-07-16 02:38:26 -0700

    added num roundtrips graph to paper

--- plots/plot.sh
+++ plots/plot.sh
@@ -1,5 +1,5 @@
 #\bin\bash
 gnuplot gnuplot_delay_over_srtf #&& display delay_over_srtf.svg
-gnuplot gnuplot_num_roundtrips && display num_roundtrips.svg
+gnuplot gnuplot_num_roundtrips #&& display num_roundtrips.svg
 inkscape delay_over_srtf.svg -A delay_over_srtf.pdf
 inkscape num_roundtrips.svg -A num_roundtrips.pdf
