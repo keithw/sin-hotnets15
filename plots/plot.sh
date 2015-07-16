ebe96c5f520411a343d5bbbb669d0759775509b4
Greg Hill <gdh39@cornell.edu>
2015-07-16 05:53:33 +0000

    added graph for start time dice size=1

--- plots/plot.sh
+++ plots/plot.sh
@@ -1,3 +1,3 @@
 #\bin\bash
-gnuplot gnuplot_delay_over_srtf && display delay_over_srtf.svg
+gnuplot gnuplot_delay_over_srtf #&& display delay_over_srtf.svg
 inkscape delay_over_srtf.svg -A delay_over_srtf.pdf
