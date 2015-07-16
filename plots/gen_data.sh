61c5a39688b86765a27cfdc6fd3b9868d5dac8cd
Greg Hill <gdh39@cornell.edu>
2015-07-15 22:08:37 -0700

    script works with two die

--- plots/gen_data.sh
+++ plots/gen_data.sh
@@ -2,8 +2,9 @@ SIN_LOCATION=../../sin/src/sin
 DELAY_RATIO_FILE=delay_over_srtf.dat
 #SRTF_RATIO_FILE=percent_match_srtf.dat
 
-NUM_TRIALS=100
-DIE_SIZE=40
+NUM_TRIALS=10
+START_TIME_DIE_SIZE=1
+FLOW_LENGTH_DIE_SIZE=40
 
 echo '"num users" "Optimal" "SIN" "ignore me" "Rate Sharing"' > $DELAY_RATIO_FILE
 #echo '"num users" "Market" "Ideal TCP"' > $SRTF_RATIO_FILE
@@ -13,7 +14,7 @@ for i in 2 3 4 6 8 16 32;
         for user_type in " " "--round-robin-user"
             do
             DATA_LOCATION=data/temp$i$user_type
-            $SIN_LOCATION --num-users=$i --num-trials=$NUM_TRIALS --die-size=$DIE_SIZE $user_type --vv > $DATA_LOCATION
+            $SIN_LOCATION --num-users=$i --num-trials=$NUM_TRIALS --start-time-die-size=$START_TIME_DIE_SIZE --flow-length-die-size=$FLOW_LENGTH_DIE_SIZE $user_type --vv > $DATA_LOCATION
 
            cat $DATA_LOCATION | grep "average flow duration for srtf" | sed s/'average flow duration for srtf'/' '/g | tr -d '\n' >> $DELAY_RATIO_FILE
            cat $DATA_LOCATION | grep "average flow duration for trials" | sed s/'average flow duration for trials'/' '/g | tr -d '\n' >> $DELAY_RATIO_FILE
