fea7b260511c3014861bbf6bbc58bfeaff8f3fe3
Greg Hill <gdh39@cornell.edu>
2015-07-16 02:09:51 -0700

    script to get average number of round trips to plot-ready format

--- plots/gen_data.sh
+++ plots/gen_data.sh
@@ -1,16 +1,17 @@
 SIN_LOCATION=../../sin/src/sin
 DELAY_RATIO_FILE=delay_over_srtf.dat
-#SRTF_RATIO_FILE=percent_match_srtf.dat
+NUM_ROUNDTRIPS_FILE=num_roundtrips.dat
 
-NUM_TRIALS=1000
+NUM_TRIALS=1
 START_TIME_DIE_SIZE=40
 FLOW_LENGTH_DIE_SIZE=40
 
 echo '"num users" "Optimal" "SIN" "ignore me" "Rate Sharing"' > $DELAY_RATIO_FILE
-#echo '"num users" "Market" "Ideal TCP"' > $SRTF_RATIO_FILE
+echo '"num users" "Mean market round trips per user" "ignore"' > $NUM_ROUNDTRIPS_FILE
 for i in 2 3 4 6 8 16 32;
         do
         echo -n "$i" >> $DELAY_RATIO_FILE
+        echo -n "$i" >> $NUM_ROUNDTRIPS_FILE 
         for user_type in " " "--round-robin-user"
             do
             DATA_LOCATION=data/temp$i$user_type
@@ -18,8 +19,11 @@ for i in 2 3 4 6 8 16 32;
 
            cat $DATA_LOCATION | grep "average flow duration for srtf" | sed s/'average flow duration for srtf'/' '/g | tr -d '\n' >> $DELAY_RATIO_FILE
            cat $DATA_LOCATION | grep "average flow duration for trials" | sed s/'average flow duration for trials'/' '/g | tr -d '\n' >> $DELAY_RATIO_FILE
+
+           cat $DATA_LOCATION | grep "average number of roundtrips per user" | sed s/'average number of roundtrips per user'/' '/g | tr -d '\n' >> $NUM_ROUNDTRIPS_FILE 
             done
 
         echo "" >> $DELAY_RATIO_FILE
+        echo "" >> $NUM_ROUNDTRIPS_FILE 
         done
 #rm temp
