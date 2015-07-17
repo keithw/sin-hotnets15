3621ea3e0d7abb3e579fc929f3d9a988169a7ba9
Greg Hill <gdh39@cornell.edu>
2015-07-16 23:55:39 -0700

    python script to parse files

---
+++ plots/data/parse_output_to_utility.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from collections import defaultdict
+import numpy as np
+import sys
+
+with open(sys.argv[1], 'r') as input_file:
+    srtf_dict = defaultdict(list)
+    simulated_dict = defaultdict(list)
+    for line in input_file:
+        srtf = line.count("in srtf schedule") > 0
+        simulated = line.count("in simulated schedule") > 0
+        if srtf or simulated:
+            nums = []
+            for word in line.split():
+                try:
+                    nums.append(float(word))
+                except ValueError:
+                    pass
+
+            assert( len( nums ) == 2 )
+            assert( not srtf or not simulated )
+            if srtf:
+                srtf_dict[int(nums[0])].append(nums[1])
+            if simulated:
+                simulated_dict[int(nums[0])].append(nums[1])
+    with open("flow_benefits.dat", 'w') as output_file:
+        for i in range(1,40):
+            output_file.write(str(i) + " " + str(np.mean(srtf_dict[i])) + " " + str(np.mean(simulated_dict[i])) + "\n")
